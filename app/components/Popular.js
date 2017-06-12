8469f790d5aad6bba180e17377f06854b63f8bc1
jace45 <jamestephens09@gmail.com>
2017-06-12 15:07:41 -0500

    "Jayson | added updateSelectedLanguage and state to component"

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,11 +1,36 @@
 var React = require('react');
 
 class Popular extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedLanguage: 'All'
+        };
+
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    updateLanguage(lang) {
+        this.setState(() => {
+            return {
+                selectedLanguage: lang
+            }
+        });
+    }
+
     render() {
+        var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
         return (
-            <div>
-                Popular!
-            </div>
+            <ul className="languages">
+                {languages.map(lang => (
+                    <li
+                        style = {lang === this.state.selectedLanguage ? {color: "#d0021b"}: null}
+                        key={lang}
+                        onClick={this.updateLanguage.bind(null, lang)} >
+                        {lang}
+                    </li>
+                ))}
+            </ul>
         )
     }
 }

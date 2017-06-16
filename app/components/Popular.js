d512b9a0a36bed792618c6e3f8b535a91fc627d7
jace45 <jamestephens09@gmail.com>
2017-06-16 10:41:48 -0500

    "Jayson | added RepoGrid to Popular.js"

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -16,6 +16,36 @@ const SelectLanguage = (props) => {
             ))}
         </ul>
     )
+};
+
+function RepoGrid (props) {
+    return (
+        <ul className='popular-list'>
+            {props.repos.map(function (repo, index) {
+                return (
+                    <li key={repo.name} className='popular-item'>
+                        <div className='popular-rank'>#{index + 1}</div>
+                        <ul className='space-list-items'>
+                            <li>
+                                <img
+                                    className='avatar'
+                                    src={repo.owner.avatar_url}
+                                    alt={'Avatar for ' + repo.owner.login}
+                                />
+                            </li>
+                            <li><a href={repo.html_url}>{repo.name}</a></li>
+                            <li>@{repo.owner.login}</li>
+                            <li>{repo.stargazers_count} stars</li>
+                        </ul>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+RepoGrid.propTypes = {
+    repos: PropTypes.array.isRequired,
 }
 
 SelectLanguage.propTypes = {
@@ -40,8 +70,7 @@ class Popular extends React.Component {
     updateLanguage(lang) {
         this.setState(function () {
             return {
-                selectedLanguage: lang,
-                repos: null
+                selectedLanguage: lang
             }
         });
 
@@ -62,6 +91,9 @@ class Popular extends React.Component {
                     selectedLanguage = {this.state.selectedLanguage}
                     onSelect = {this.updateLanguage}
                 />
+                {!this.state.repos
+                    ? <p>LOADING!</p>
+                    : <RepoGrid repos={this.state.repos} />}
             </div>
         )
     }

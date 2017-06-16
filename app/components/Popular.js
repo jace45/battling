29e69fd9076ac8d6494abbb97216fcf79158e87f
jace45 <jamestephens09@gmail.com>
2017-06-15 22:14:22 -0500

    Jayson | added lifecycle for api call

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 const SelectLanguage = (props) => {
     var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -26,18 +27,32 @@ class Popular extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos:  null
         };
 
         this.updateLanguage = this.updateLanguage.bind(this);
     }
 
+    componentDidMount() {
+        this.updateLanguage(this.state.selectedLanguage)
+    }
     updateLanguage(lang) {
-        this.setState(() => {
+        this.setState(function () {
             return {
-                selectedLanguage: lang
+                selectedLanguage: lang,
+                repos: null
             }
         });
+
+        api.fetchPopularRepos(lang)
+            .then(function (repos) {
+                this.setState(function () {
+                    return {
+                        repos: repos
+                    }
+                });
+            }.bind(this));
     }
 
     render() {

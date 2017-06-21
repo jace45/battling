cbb0f6f18e5eb0461a92beec4434c07042be48f1
jace45 <jamestephens09@gmail.com>
2017-06-21 09:12:03 -0500

    Jayson | got ReactRouter working and added Navigation along with ApiFallback

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import Popular from './Popular';
+import Nav from './Nav';
 
 class App extends React.Component {
     render() {
         return (
-            <div className="container">
-                <Popular />
-            </div>
+            <Router>
+                <div className="container">
+                    <Nav />
+                    <Route path="/popular" component={Popular} />
+                </div>
+            </Router>
         )
     }
 }

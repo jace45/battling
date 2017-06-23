1d3061fe530d4164fc979de68b734b13ccac8bea
jace45 <jamestephens09@gmail.com>
2017-06-23 13:35:43 -0500

    Jayson | added battle component to app page

--- app/components/App.js
+++ app/components/App.js
@@ -3,6 +3,8 @@ import {Route} from 'react-router-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Popular from './Popular';
 import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
 
 class App extends React.Component {
     render() {
@@ -10,6 +12,8 @@ class App extends React.Component {
             <Router>
                 <div className="container">
                     <Nav />
+                    <Route exact path="/" component={Home} />
+                    <Route path="/battle" component={Battle} />
                     <Route path="/popular" component={Popular} />
                 </div>
             </Router>

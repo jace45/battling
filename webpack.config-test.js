0501d5101d3dbc93a7d824ef85d9099226216dc5
jace45 <jamestephens09@gmail.com>
2017-07-05 13:31:32 -0500

    Jayson | experiementing with package.json

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -1,6 +1,22 @@
 var nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    target: 'node', // in order to ignore built-in modules like path, fs, etc.
-    externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+    target: 'node',
+    externals: [nodeExternals()],
+
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+
+    module: {
+        loaders: [
+            { test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/ },
+            { test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/ },
+        ]
+    }
 };
+

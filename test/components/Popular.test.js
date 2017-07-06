2965b346b7c65dafb9ae9ab3a98350d7bd3d977a
jace45 <jamestephens09@gmail.com>
2017-07-06 16:54:30 -0500

    Jayson | testing works now with enzyme

--- test/components/Popular.test.js
+++ test/components/Popular.test.js
@@ -1,9 +1,13 @@
-
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Popular, { SelectLanguage }  from '../../src/components/Popular';
 
 describe('Should render Popular page', function() {
 
     it('empty test should run successfully', function() {
 
-        assert.equal('A', 'A');
+        const wrapper = shallow(<Popular />);
+        expect(wrapper.find(SelectLanguage)).to.have.length(1);
     });
 });
